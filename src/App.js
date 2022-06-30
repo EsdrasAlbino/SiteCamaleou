f38ef092248877cfe052f56a3a992ba4eff970cc
Aluno <aluno@naverecife.local>
2022-06-30 14:54:30 -0300

    Adicionando HTML do site

--- src/App.js
+++ src/App.js
@@ -5,19 +5,27 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="Jogo-logo" alt="logo" />
       </header>
+      <div>
+        <h1>A Expressão das Cores</h1>
+        <p>Usamos cores para expressar diversos sentimentos dos jogadores.</p>
+        <img src={logo}></img>
+      </div>
+      <div>
+        <h1>História</h1>
+        <p>Mono é morador de uma vila que se afundou na tristeza e depressão, entretanto, Mono percebe algo diferente no céu, uma luz, enquanto tudo ao seu redor é preto e branco.</p>
+        <img src={logo}></img>
+      </div>
+      <div>
+        <h1>Personagem</h1>
+        <p>MONO</p>
+        <img src={logo}></img>
+      </div>
+      <div>
+        <h1>Mecânicas</h1>
+
+      </div>
     </div>
   );
 }
